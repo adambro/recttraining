a6ef4b38c43c2c6d7e8fa139df12db31d23f320e
Adam Brodziak <adambro@o2.pl>
2017-11-23 10:36:13 +0100

    Zad 1 - done

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 class MainComponent extends React.Component {
+    render () {
+        return <MainScene>Hello world</MainScene>;
+    }
 }
 
-const MainScene = <MainComponent>Hello world!</MainComponent>;
+const MainScene = (props) => {
+    return (<div>{props.children}</div>);
+}
 
-MainComponent.PropTypes = {
+MainComponent.propTypes = {
     todos: PropTypes.string.isRequired
 }
 
-ReactDOM.render(<MainComponent />, document.getElementById('app'));
+ReactDOM.render(<MainComponent />, document.getElementById('root'));
 registerServiceWorker();
